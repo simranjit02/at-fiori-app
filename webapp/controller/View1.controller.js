b1b46768a0289f536babcf37bd7bd8a066507933
Simranjit Singh <simranjitsingh58648@gmail.com>
2025-06-08 11:39:34 +0000

    integrate filters fnctionality

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,14 +1,29 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  return Controller.extend("sm.dev.tut.controller.View1", {
-    onInit: function () {
-      console.log("View1");
-    },
-    gotoView2: function () {
-      var parentCompo = this.getView().getParent();
-      parentCompo.to("View2");
-    },
-    filterFruit: function(){
-      
-    }
-  });
-});
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+  ],
+  function (Controller, Filter, FilterOperator) {
+    return Controller.extend("sm.dev.tut.controller.View1", {
+      onInit: function () {},
+      gotoView2: function () {
+        var parentCompo = this.getView().getParent();
+        parentCompo.to("View2");
+      },
+      filterFruit: function (oEvent) {
+        var oQuery = oEvent.getParameter("newValue");
+        var nFilter = new Filter("name", FilterOperator.Contains, oQuery);
+        var tFilter = new Filter("taste", FilterOperator.Contains, oQuery);
+        var aFilter = [nFilter, tFilter];
+        var masterFilter = new Filter({
+          filters: aFilter,
+          and: false,
+        });
+        var oList = this.getView().byId("idFruitList");
+        oList.getBinding("items").filter(masterFilter);
+        console.log(aFilter);
+      },
+    });
+  }
+);
